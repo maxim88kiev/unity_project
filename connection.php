c389ef1cab52360bd4a81baa753dc96452dde1c0
maxim <maxim88kiev@gmail.com>
2018-12-07 22:58:08 +0200

    Delete connection.php

--- connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = 'gagames.mysql.tools'; // адрес сервера 
-$database = 'gagames_db'; // имя базы данных
-$user = 'gagames_db'; // имя пользователя
-$password = 'jcJzzdB7'; // пароль
-?>
